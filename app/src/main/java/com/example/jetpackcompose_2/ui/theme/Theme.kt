211b4c5d4e2d0534e7e2b12397f1356e9bd09544
vicarnrom <vicarnrom@alu.edu.gva.es>
2023-10-23 12:34:25 +0200

    suuu

---
+++ app/src/main/java/com/example/jetpackcompose_2/ui/theme/Theme.kt
@@ -0,0 +1,67 @@
+package com.example.jetpackcompose_2.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.ViewCompat
+
+val pink = Color(0xFFf590ff)
+val yellow = Color(0xFFf8ff60)
+val green = Color(0xFFA8FF60)
+val grey = Color(0xFFb4b4b4)
+
+private val DarkColorScheme = darkColorScheme(
+    surface = yellow,
+    onSurface = pink,
+    primary = pink,
+    onPrimary = grey
+)
+
+private val LightColorScheme = lightColorScheme(
+    surface = yellow,
+    onSurface = Color.White,
+    primary = green,
+    onPrimary = pink
+)
+
+@Composable
+fun JetpackCompose_2Theme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
+            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
